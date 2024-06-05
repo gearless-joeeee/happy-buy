aa5c013c0c704a30485a33fd5b5784b76ebc51ff
gearless-joeeee <bayesianifeg1@gmail.com>
2024-06-05 16:44:56 +0800

    feature(order page): 集成ant mobile pciker组件

--- src/components/Order/index.jsx
+++ src/components/Order/index.jsx
@@ -4,6 +4,8 @@ import './style.scss'
 import { useParams } from 'react-router-dom'
 import message from '../../utils/message'
 import Popover from '../Popover'
+import { Picker } from 'antd-mobile'
+
 function Order() {
   // 请求订单页数据
   const [data, setData] = useState(null)
@@ -16,57 +18,68 @@ function Order() {
       params: {
         id: params.orderId,
       },
-    }).then((response) => {
-      if (response.success) {
-        setData(response.data)
-      }
-    }).catch(e=>message(e.message))
+    })
+      .then((response) => {
+        if (response.success) {
+          setData(response.data)
+        }
+      })
+      .catch((e) => message(e.message))
   }, [request, params])
-
 
   // 地址列表弹窗
   const [showAddressList, setShowAddressList] = useState(false)
   const [addressList, setAddressList] = useState([])
-  const {request: addressListRequest} = useRequest({manual:true})
-  function handleReceiverClick(){
+  const { request: addressListRequest } = useRequest({ manual: true })
+  function handleReceiverClick() {
     addressListRequest({
-      url:'/userAddress',
-      method:'GET'
-    }).then(response=>{
-      if(response.success){
-        setAddressList(response.data)
-        setShowAddressList(true)
-      }
-    }).catch(e=>message(e.message))
+      url: '/userAddress',
+      method: 'GET',
+    })
+      .then((response) => {
+        if (response.success) {
+          setAddressList(response.data)
+          setShowAddressList(true)
+        }
+      })
+      .catch((e) => message(e.message))
   }
-  function handleAddressClick(address){
-    const newData = {...data}
-     newData.address = {
-      id:address.id,
+  function handleAddressClick(address) {
+    const newData = { ...data }
+    newData.address = {
+      id: address.id,
       name: address.name,
       phone: address.phone,
-      address: address.address
+      address: address.address,
     }
     setShowAddressList(false)
     setData(newData)
   }
+
+  // 送达日期
+  const [showTimeRange, setShowTimeRange] = useState(false)
+
   return data ? (
     <div className="page page-order">
       <h3 className="title">确认订单</h3>
-      <div 
-        className="receiver"
-        onClick={handleReceiverClick}
-      >
+      <div className="receiver" onClick={handleReceiverClick}>
         <span className="iconfont receiver-icon">&#xe650;</span>
         <div className="receiver-content">
-          <span className="receiver-content-name">收货人:{data.address.name}</span>
+          <span className="receiver-content-name">
+            收货人:{data.address.name}
+          </span>
           <span className="receiver-content-phone">{data.address.phone}</span>
-          <p className="receiver-content-address">收货人地址: {data.address.address}</p>
+          <p className="receiver-content-address">
+            收货人地址: {data.address.address}
+          </p>
         </div>
       </div>
       <div className="delivery">
         <span className="delivery-text">送达时间</span>
-        <span className="delivery-select">
+        <span 
+          className="delivery-select"
+          onClick={()=>setShowTimeRange(true)}
+        >
           {data.time?.[0]} {data.time?.[1]}:{data.time?.[2]}
         </span>
       </div>
@@ -93,16 +106,14 @@ function Order() {
                       {product?.weight}
                     </p>
                   </div>
-                  <div className="shop-product-order">         
-                  <p className="shop-product-order-price">
-                    <span className="shop-product-content-price-yen">
-                      &yen;
-                    </span>
-                    {product?.price}
-                  </p>
-                  <p className="shop-product-order-count">
-                    x{product.count}
-                  </p>
+                  <div className="shop-product-order">
+                    <p className="shop-product-order-price">
+                      <span className="shop-product-content-price-yen">
+                        &yen;
+                      </span>
+                      {product?.price}
+                    </p>
+                    <p className="shop-product-order-count">x{product.count}</p>
                   </div>
                 </li>
               )
@@ -121,30 +132,48 @@ function Order() {
         </p>
         <button className="order-submit-btn">提交订单</button>
       </div>
-      <Popover show={showAddressList} maskClickCallback={()=> setShowAddressList(false)}>
+      <Popover
+        show={showAddressList}
+        maskClickCallback={() => setShowAddressList(false)}
+      >
         <ul className="address-popover">
           <h3 className="address-popover-title">选择地址</h3>
-          {
-            addressList.map(address=>{
-              return (
-                <li 
-                  className="address-popover-item"
-                  key={address.id}
-                  onClick={()=>handleAddressClick(address)}
-                >
-                  <span className="address-popover-item-name">{address.name}</span>
-                  <span className="address-popover-item-phone">{address.phone}</span>
-                  <p className="address-popover-item-address">
-                    收货人地址: {address.address}
-                  </p>
-                </li>
-              )
-            })
-          }
+          {addressList.map((address) => {
+            return (
+              <li
+                className="address-popover-item"
+                key={address.id}
+                onClick={() => handleAddressClick(address)}
+              >
+                <span className="address-popover-item-name">
+                  {address.name}
+                </span>
+                <span className="address-popover-item-phone">
+                  {address.phone}
+                </span>
+                <p className="address-popover-item-address">
+                  收货人地址: {address.address}
+                </p>
+              </li>
+            )
+          })}
         </ul>
       </Popover>
+      <Picker
+        visible={showTimeRange}
+        columns={data.timeRange}
+        value={data.time}
+        onConfirm={(value)=> {
+          const newData = {...data}
+          newData.time = value
+          setData(newData)
+          setShowTimeRange(false)
+        }}
+        onSelect={(val, extend) => {
+          console.log('onSelect', val, extend.items)
+        }}
+      />
     </div>
-
   ) : null
 }
 
