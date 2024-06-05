a2109c0abf5fc6da3183d8e4bfb9ab299876c8b3
gearless-joeeee <bayesianifeg1@gmail.com>
2024-06-05 16:32:58 +0800

    feature(order page): 地址列表弹窗实现

--- src/components/Order/index.jsx
+++ src/components/Order/index.jsx
@@ -3,6 +3,7 @@ import useRequest from '../../hooks/useRequest'
 import './style.scss'
 import { useParams } from 'react-router-dom'
 import message from '../../utils/message'
+import Popover from '../Popover'
 function Order() {
   // 请求订单页数据
   const [data, setData] = useState(null)
@@ -23,11 +24,39 @@ function Order() {
   }, [request, params])
 
 
-
+  // 地址列表弹窗
+  const [showAddressList, setShowAddressList] = useState(false)
+  const [addressList, setAddressList] = useState([])
+  const {request: addressListRequest} = useRequest({manual:true})
+  function handleReceiverClick(){
+    addressListRequest({
+      url:'/userAddress',
+      method:'GET'
+    }).then(response=>{
+      if(response.success){
+        setAddressList(response.data)
+        setShowAddressList(true)
+      }
+    }).catch(e=>message(e.message))
+  }
+  function handleAddressClick(address){
+    const newData = {...data}
+     newData.address = {
+      id:address.id,
+      name: address.name,
+      phone: address.phone,
+      address: address.address
+    }
+    setShowAddressList(false)
+    setData(newData)
+  }
   return data ? (
     <div className="page page-order">
       <h3 className="title">确认订单</h3>
-      <div className="receiver">
+      <div 
+        className="receiver"
+        onClick={handleReceiverClick}
+      >
         <span className="iconfont receiver-icon">&#xe650;</span>
         <div className="receiver-content">
           <span className="receiver-content-name">收货人:{data.address.name}</span>
@@ -92,7 +121,30 @@ function Order() {
         </p>
         <button className="order-submit-btn">提交订单</button>
       </div>
+      <Popover show={showAddressList} maskClickCallback={()=> setShowAddressList(false)}>
+        <ul className="address-popover">
+          <h3 className="address-popover-title">选择地址</h3>
+          {
+            addressList.map(address=>{
+              return (
+                <li 
+                  className="address-popover-item"
+                  key={address.id}
+                  onClick={()=>handleAddressClick(address)}
+                >
+                  <span className="address-popover-item-name">{address.name}</span>
+                  <span className="address-popover-item-phone">{address.phone}</span>
+                  <p className="address-popover-item-address">
+                    收货人地址: {address.address}
+                  </p>
+                </li>
+              )
+            })
+          }
+        </ul>
+      </Popover>
     </div>
+
   ) : null
 }
 
